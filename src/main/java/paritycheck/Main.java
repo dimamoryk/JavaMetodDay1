edb15a38ef8317bdda4d0c7f4741157281c744a4
Dmitri <dimamoryk@gmail.com>
2026-06-06 20:38:17 +0300

    modified files

--- src/main/java/paritycheck/Main.java
+++ src/main/java/paritycheck/Main.java
@@ -2,13 +2,13 @@ package paritycheck;
 
 public class Main {
 
-    public static boolean isEvan(int n) {
+    public static boolean isEven(int n) {
         return n % 2 == 0;
     }
 
     public static void main(String... args) {
 
-        System.out.println(isEvan(10)); // true
-        System.out.println(isEvan(7)); // false
+        System.out.println(isEven(10)); // true
+        System.out.println(isEven(7)); // false
     }
 }
